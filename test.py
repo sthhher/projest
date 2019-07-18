ad8a677c5f6479aa53d95dc06c6101381381f375
sthhher <esvemira@hotmail.com>
2019-07-18 17:14:34 +0200

    add tests

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestProject(unittest.TestCase):
+
+    def test_get_sequence_aminoacids(self):
+        pass
+
+    def test_get_chain_list(self):
+        pass
+
+    def test_get_aminoacid_list(self):
+        pass
+
+    def test_get_similar_protein(self):
+        pass
+
+    def test_general_dictionary(self):
+        pass
+
+    def test_load_protein_pdb(self):
+        pass
+
+    def test_load_protein_fasta(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
