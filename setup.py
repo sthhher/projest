fea6a2201538f245bbdbe555e58895819748901a
sthhher <esvemira@hotmail.com>
2019-07-19 16:20:55 +0200

    extract_atoms_information.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -12,21 +11,7 @@ setuptools.setup(
     description='A random test lib',
     author='Esther',
     author_email='esvemira@hotmail.com',
-    url='https://github.com/sthhher/eproj', # Usa la URL del repositorio de GitHub
+    url='https://github.com/sthhher/projest', # Usa la URL del repositorio de GitHub
     keywords='test example develop', # Palabras que definan tu paquete
     classifiers=['Programming Language :: Python'],
-=======
-from distutils.core import setup
-setup(
-    name = 'mypackage',
-    packages = ['mypackage'], # this must be the same as the name above
-    version = '0.1',
-    description = 'Pass the file of github to pipy',
-    author = 'Esther Vendrell',
-    author_email = 'esvemira@hotmail.com',
-    url = 'https://github.com/sthhher/projest', # use the URL to the github repo
-    download_url = 'https://github.com/sthhher/projest/tarball/0.1',
-    keywords = ['testing', 'logging', 'example'],
-    classifiers = [],
->>>>>>> 385e2b8d45c70a2f7876ab852418c3344cc9b2c0
 )
