f9e09066ee7f598fffeb93a161f56e18281eed13
sthhher <esvemira@hotmail.com>
2019-07-22 11:23:53 +0200

    extract_atoms_information.py

---
+++ projest_inf/chain.py
@@ -0,0 +1,8 @@
+
+class Chain:
+    
+    def __init__(self, chain_identifier):
+        self.chain_identifier = chain_identifier
+        
+    def __str__(self):
+        return "%s" %(self.chain_identifier)
