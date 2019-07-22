edef41ed64ef5d6ad56e8f54c914890aed906c38
sthhher <51945891+sthhher@users.noreply.github.com>
2019-07-22 15:37:18 +0200

    Delete chain.py

--- build/lib/projest/chain.py
+++
@@ -1,8 +0,0 @@
-
-class Chain:
-    
-    def __init__(self, chain_identifier):
-        self.chain_identifier = chain_identifier
-        
-    def __str__(self):
-        return "%s" %(self.chain_identifier)
